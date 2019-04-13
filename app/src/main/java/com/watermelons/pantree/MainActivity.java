c413e95dfa629011d333448b52fe8b0f3f36219b
mcherna2 <46982245+mcherna2@users.noreply.github.com>
2019-04-13 14:50:53 -0700

    completed functions for taking an image and parsing its text, lines of code about taking a picture are commented out

--- app/src/main/java/com/watermelons/pantree/MainActivity.java
+++ app/src/main/java/com/watermelons/pantree/MainActivity.java
@@ -1,15 +1,51 @@
 package com.watermelons.pantree;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.icu.text.IDNA;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.document.FirebaseVisionDocumentText;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Integer maxHeight;
+    private Integer maxWidth;
+    private FloatingActionButton fab;
+    private android.content.Context x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +53,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = findViewById(R.id.fab);
+        fab = findViewById(R.id.fab);
+        x = this;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Bitmap image = getBitmapFromAsset(x, "grocery-receipt-2.jpg");
+                runTextRecognition(image);
+                //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //startActivityForResult(intent, 0);
             }
         });
     }
+
+    /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == 1111 && resultCode == RESULT_OK) {
+            super.onActivityResult(requestCode, resultCode, data);
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+
+        }
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,4 +96,59 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void runTextRecognition(Bitmap bitmap){
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionTextRecognizer recognizer = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
+        fab.setEnabled(false);
+        recognizer.processImage(image).addOnSuccessListener(
+                new OnSuccessListener<FirebaseVisionText>() {
+                    @Override
+                    public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                        fab.setEnabled(true);
+                        processTextRecognition(firebaseVisionText);
+                    }
+                }).addOnFailureListener(
+                new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        fab.setEnabled(true);
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+    private void processTextRecognition(FirebaseVisionText firebaseVisionText){
+        List<FirebaseVisionText.TextBlock> blocks = firebaseVisionText.getTextBlocks();
+        if(blocks.size() == 0){
+            return;
+        }
+        else{
+            for(int i = 0; i < blocks.size(); i++){
+                List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
+                for(int j = 0; j < lines.size(); j++){
+                    String text = lines.get(j).getText();
+                    Log.i("Info", text);
+                }
+
+            }
+        }
+    }
+
+    public static Bitmap getBitmapFromAsset(Context context, String filePath) {
+        AssetManager assetManager = context.getAssets();
+
+        InputStream is;
+        Bitmap bitmap = null;
+        try {
+            is = assetManager.open(filePath);
+            bitmap = BitmapFactory.decodeStream(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return bitmap;
+    }
+
+
 }
